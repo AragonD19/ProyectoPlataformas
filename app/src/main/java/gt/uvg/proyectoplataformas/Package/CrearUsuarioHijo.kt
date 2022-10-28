d02783c54dfa62402cf8f60f0803fb0be0960dd6
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-10-27 20:03:17 -0600

    Inicio de navegacion

--- app/src/main/java/gt/uvg/proyectoplataformas/Package/CrearUsuarioHijo.kt
+++ app/src/main/java/gt/uvg/proyectoplataformas/Package/CrearUsuarioHijo.kt
@@ -1,4 +1,4 @@
-package gt.uvg.proyectoplataformas.Packge
+package gt.uvg.proyectoplataformas.Package
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CrearUsuarioPadre.newInstance] factory method to
+ * Use the [CrearUsuarioHijo.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CrearUsuarioPadre : Fragment() {
+class CrearUsuarioHijo : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CrearUsuarioPadre : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_usuario_padre, container, false)
+        return inflater.inflate(R.layout.fragment_crear_usuario_hijo, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CrearUsuarioPadre : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CrearUsuarioPadre.
+         * @return A new instance of fragment CrearUsuarioHijo.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CrearUsuarioPadre().apply {
+            CrearUsuarioHijo().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
