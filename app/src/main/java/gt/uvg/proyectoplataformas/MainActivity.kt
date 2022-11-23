cf29122b71788b689abc8eb09280c76e5c0f7ef8
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-11-23 16:30:57 -0600

    Solucino de problemas

--- app/src/main/java/gt/uvg/proyectoplataformas/MainActivity.kt
+++ app/src/main/java/gt/uvg/proyectoplataformas/MainActivity.kt
@@ -2,28 +2,13 @@ package gt.uvg.proyectoplataformas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
 
-import gt.uvg.proyectoplataformas.Package.CrearTarea
-import gt.uvg.proyectoplataformas.Package.MenuHijo
-import gt.uvg.proyectoplataformas.Package.MenuPadre
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        replaceFragment(MenuPadre())
 
-
-    }
-
-    private fun replaceFragment(menuHijo: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainerView2,menuHijo)
-        fragmentTransaction.commit()
 
     }
 }
