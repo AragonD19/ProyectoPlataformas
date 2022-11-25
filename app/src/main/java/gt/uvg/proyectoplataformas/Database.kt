964f379600d9033c3cca83df63e7bc774da787cd
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-11-24 20:18:02 -0600

    ajustes de layout

--- app/src/main/java/gt/uvg/proyectoplataformas/Database.kt
+++ app/src/main/java/gt/uvg/proyectoplataformas/Database.kt
@@ -8,11 +8,16 @@ object Database {
 
         val itemList = arrayListOf<Any>()
 
-        itemList.add(DataItem.Header("Pablo"))
-        itemList.add(DataItem.Item(1,"barrer"))
+        itemList.add(DataItem.Header("Santi"))
+        itemList.add(DataItem.Item(1,"Lavar el carro"))
+        itemList.add(DataItem.Item(2,"Sacar la basura"))
+        itemList.add(DataItem.Item(3,"barrer la casa"))
+        itemList.add(DataItem.Item(4,"lavar la ropa"))
 
-        itemList.add(DataItem.Header("Juan"))
-        itemList.add(DataItem.Item(1,"lavar los platos"))
+        itemList.add(DataItem.Header("Ivania"))
+        itemList.add(DataItem.Item(5,"Lavar el carro"))
+        itemList.add(DataItem.Item(6,"Hacer la tarea"))
+        itemList.add(DataItem.Item(7,"Lavar la ropa"))
 
 
         return itemList
